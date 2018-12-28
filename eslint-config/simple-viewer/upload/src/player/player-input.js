1d7ce30705effcdc35694da33048c9bb0bcc8cd3
Lars Wobus <lars.wobus@web.de>
2018-12-28 09:14:57 +0100

    Code cleanup

--- eslint-config/simple-viewer/upload/src/player/player-input.js
+++ eslint-config/simple-viewer/upload/src/player/player-input.js
@@ -17,27 +17,13 @@ PlayerInput.prototype.postInitialize = function () {
 // PlayerInput.prototype.update = function (dt)
 PlayerInput.prototype.syncedUpdate = function (/* dt */) {
   // Important note: this.app.keyboard.on(pc.EVENT_KEYDOWN, ..., this) would work, but its not smooth enough
-  const { _keyboard, _controller, _player } = this;
+  const { _keyboard } = this;
   const forward = _keyboard.isPressed(pc.KEY_UP);
   const backward = _keyboard.isPressed(pc.KEY_DOWN);
   const left = _keyboard.isPressed(pc.KEY_LEFT);
   const right = _keyboard.isPressed(pc.KEY_RIGHT);
   const jump = _keyboard.isPressed(pc.KEY_SPACE);
 
-  /* _player.tryToJump(jump);
-  _player.tryToRunLeft(left && !right);
-  _player.tryToRunRight(!left && right);
-  _player.tryToRunForward(forward && !backward);
-  _player.tryToRunBackward(!forward && backward); */
-  // this.forwards += (forward && !backward) ? 0.1 : -0.05;
-  // this.backwards += (!forward && backward) ? 0.1 : -0.05;
-  // this.left += (left && !right) ? 0.1 : -0.05;
-  // this.right += (!left && right) ? 0.1 : -0.05;
-  // this.upwards += (jump) ? 0.1 : -0.1;
-
-  // if (forward) this._propulsion.forward();
-  // if (jump) this._propulsion.upward();
-  // if (left) this._propulsion.sideward();
   if (forward && !backward) {
     this._propulsion.intensityZ = 1;
   } else if (!forward && backward) {
@@ -55,6 +41,4 @@ PlayerInput.prototype.syncedUpdate = function (/* dt */) {
   } else {
     this._propulsion.intensityX = 0;
   }
-
-  // _player.tryToIdle();
 };
