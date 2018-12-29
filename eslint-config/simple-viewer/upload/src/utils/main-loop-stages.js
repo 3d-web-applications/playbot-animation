8fb1726eba62029c2a16dd4d86f8ed244529d42e
Lars Wobus <lars.wobus@web.de>
2018-12-29 23:49:22 +0100

    New design for mainloop

---
+++ eslint-config/simple-viewer/upload/src/utils/main-loop-stages.js
@@ -0,0 +1,13 @@
+const VisitInput = 0;
+const InterpretState = 1;
+const PerformAction = 2;
+const EvaluateChange = 3;
+const RefreshScreen = 4;
+
+export {
+  VisitInput,
+  InterpretState,
+  PerformAction,
+  EvaluateChange,
+  RefreshScreen,
+};
--- eslint-config/simple-viewer/upload/src/utils/main-loop.js
+++ eslint-config/simple-viewer/upload/src/utils/main-loop.js
@@ -4,80 +4,37 @@ import { registerOnFocusLost } from './on-focus-lost';
 
 const MainLoop = pc.createScript('MainLoop');
 
-MainLoop.prototype._userInputs = [];
-MainLoop.prototype._memory = [];
-MainLoop.prototype._controllers = [];
-MainLoop.prototype._processors = [];
-MainLoop.prototype._animators = [];
+MainLoop.prototype._subscribers = [
+  [], [], [], [], [],
+];
 
 MainLoop.instance = null;
 
 MainLoop.prototype.initialize = function () {
+  // Allow registration from any instance, but updates only from the first one
   if (MainLoop.instance) {
-    // console.error('MainLoop already exist');
-    this.addUserInput = MainLoop.instance.addUserInput;
-    // this.addStateMemory = MainLoop.instance.addStateMemory;
-    this.addController = MainLoop.instance.addController;
-    this.addStateProcessor = MainLoop.instance.addStateProcessor;
-    this.addAnimator = MainLoop.instance.addAnimator;
+    this.registerFunction = MainLoop.instance.registerFunction;
     this.enabled = false;
     return;
   }
 
   MainLoop.instance = this;
 
+  // Primary loop can be paused and resumed
   registerOnFocusFound(() => { this.enabled = true; });
   registerOnFocusLost(() => { this.enabled = false; });
 };
 
-MainLoop.prototype.addUserInput = function (fn) {
-  return addToRegistry(fn, this._userInputs);
-};
-
-/* MainLoop.prototype.addStateMemory = function (fn) {
-  return addToRegistry(fn, this._memory);
-}; */
-
-MainLoop.prototype.addController = function (fn) {
-  return addToRegistry(fn, this._controllers);
-};
-
-MainLoop.prototype.addStateProcessor = function (fn) {
-  return addToRegistry(fn, this._processors);
-};
-
-MainLoop.prototype.addAnimator = function (fn) {
-  return addToRegistry(fn, this._animators);
+MainLoop.prototype.registerFunction = function (fn, stage) {
+  addToRegistry(fn, this._subscribers[stage]);
 };
 
 MainLoop.prototype.update = function (dt) {
-  this._userInputs.forEach((syncedUpdate) => {
-    syncedUpdate(dt);
-  });
-  /* this._memory.forEach((fn) => {
-    fn(dt);
-  }); */
-  this._controllers.forEach((syncedUpdate) => {
-    syncedUpdate(dt);
-  });
-  this._processors.forEach((fn) => {
-    fn(dt);
-  });
-  this._animators.forEach((syncedUpdate) => {
-    syncedUpdate(dt);
-  });
+  this._subscribers.forEach(
+    array => array.forEach(fn => fn(dt)),
+  );
 };
 
-const addUserInput = script => MainLoop.instance.addUserInput(script);
-// const addStateMemory = script => MainLoop.instance.addStateMemory(script); // update source state
-const addController = script => MainLoop.instance.addController(script);
-const addStateProcessor = script => MainLoop.instance.addStateProcessor(script); // update sink state / end state
-const addAnimator = script => MainLoop.instance.addAnimator(script);
-
-export {
-  addUserInput,
-  // addStateMemory,
-  addController,
-  addStateProcessor,
-  addAnimator,
+export const registerFunction = (fn, stage) => {
+  MainLoop.instance.registerFunction(fn, stage);
 };
