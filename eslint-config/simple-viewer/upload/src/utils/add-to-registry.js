3e93fb2156020c444b8f5bce5107c9ee7fd34129
Lars Wobus <lars.wobus@web.de>
2018-12-29 23:48:41 +0100

    Only one register function is required

--- eslint-config/simple-viewer/upload/src/utils/add-to-registry.js
+++ eslint-config/simple-viewer/upload/src/utils/add-to-registry.js
@@ -1,8 +1,14 @@
-export const addToRegistry = (fn, array) => {
-  if (!fn || typeof fn !== 'function') {
-    return -1;
+const addToRegistry = (fn, array) => {
+  if (typeof fn !== 'function') {
+    throw new Error('First argument is not a function');
+  }
+
+  if (!Array.isArray(array)) {
+    throw new Error('Second argument is not an array');
   }
 
   array.push(fn);
   return array.length;
 };
+
+export { addToRegistry };
