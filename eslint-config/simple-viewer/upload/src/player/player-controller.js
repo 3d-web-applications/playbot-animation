055cc3d29cdf7c53e92f81a44960607ec4a1a975
Lars Wobus <lars.wobus@web.de>
2019-01-02 23:03:44 +0100

    Refactoring: more object destructuring

--- eslint-config/simple-viewer/upload/src/player/player-controller.js
+++ eslint-config/simple-viewer/upload/src/player/player-controller.js
@@ -17,55 +17,55 @@ PlayerController.attributes.add('_animatedEntity', {
 });
 
 PlayerController.prototype.initialize = function () {
+  const {
+    PlayerInput, PlaybotLocomotion, PlaybotMotionTracking, PlaybotAnimator,
+  } = this.entity.script;
   const state = createPlayerState();
 
-  const input = this.entity.script.PlayerInput;
-  input.register(state.setForward.bind(state), 'KEY_UP');
-  input.register(state.setBackward.bind(state), 'KEY_DOWN');
-  input.register(state.setLeft.bind(state), 'KEY_LEFT');
-  input.register(state.setRight.bind(state), 'KEY_RIGHT');
-  input.register(state.setJump.bind(state), 'KEY_SPACE');
+  PlayerInput.register(state.setForward.bind(state), 'KEY_UP');
+  PlayerInput.register(state.setBackward.bind(state), 'KEY_DOWN');
+  PlayerInput.register(state.setLeft.bind(state), 'KEY_LEFT');
+  PlayerInput.register(state.setRight.bind(state), 'KEY_RIGHT');
+  PlayerInput.register(state.setJump.bind(state), 'KEY_SPACE');
 
   this._playerState = state;
 
   const { _dynamicEntity } = this;
 
-  this._motor = this.entity.script.PlaybotLocomotion;
-  this._motor.setup(_dynamicEntity.rigidbody);
+  PlaybotLocomotion.setup(_dynamicEntity.rigidbody);
 
-  this._tracking = this.entity.script.PlaybotMotionTracking;
-  this._tracking.setup(_dynamicEntity);
-  this._tracking._listener.push(this._selectActiveAnimation.bind(this));
+  PlaybotMotionTracking.setup(_dynamicEntity);
+  PlaybotMotionTracking._listener.push(this._selectActiveAnimation.bind(this));
 
   this._animationState = -1;
 
-  this._animator = this.entity.script.PlaybotAnimator;
-  this._animator.animation = this._animatedEntity.animation;
+  PlaybotAnimator.animation = this._animatedEntity.animation;
 };
 
 PlayerController.prototype.precision = 0.001;
 
 PlayerController.prototype._selectActiveAnimation = function () {
-  const { dx, dy, dz } = this._tracking;
-  console.log(dx, dy, dz);
-  if (Math.abs(dy) > this.precision) {
+  const { PlaybotMotionTracking, PlaybotAnimator } = this.entity.script;
+
+  console.log(PlaybotMotionTracking.dx, PlaybotMotionTracking.dy, PlaybotMotionTracking.dz);
+  if (Math.abs(PlaybotMotionTracking.dy) > this.precision) {
     console.log(0);
     if (this._animationState === 0) return;
     this._animationState = 0;
-    this._animator.startJumpAnimation();
+    PlaybotAnimator.startJumpAnimation();
     return;
   }
-  if (Math.abs(dx) < this.precision && Math.abs(dz) < this.precision) {
+  if (Math.abs(PlaybotMotionTracking.dx) < this.precision && Math.abs(PlaybotMotionTracking.dz) < this.precision) {
     console.log(2);
     if (this._animationState === 2) return;
     this._animationState = 2;
-    this._animator.startIdleAnimation();
+    PlaybotAnimator.startIdleAnimation();
     return;
   }
   console.log(1);
   if (this._animationState === 1) return;
   this._animationState = 1;
-  this._animator.startRunAnimation();
+  PlaybotAnimator.startRunAnimation();
 };
 
 PlayerController.prototype.postInitialize = function () {
@@ -76,23 +76,23 @@ PlayerController.prototype.syncedUpdate = function () {
   const {
     forward, backward, left, right, jump,
   } = this._playerState;
-  const { _motor } = this;
+  const { PlaybotLocomotion } = this.entity.script;
 
   if (forward && !backward) {
-    _motor.intensityZ = 1;
+    PlaybotLocomotion.intensityZ = 1;
   } else if (!forward && backward) {
-    _motor.intensityZ = -1;
+    PlaybotLocomotion.intensityZ = -1;
   } else {
-    _motor.intensityZ = 0;
+    PlaybotLocomotion.intensityZ = 0;
   }
 
-  _motor.intensityY = (jump) ? 1 : 0;
+  PlaybotLocomotion.intensityY = (jump) ? 1 : 0;
 
   if (left && !right) {
-    _motor.intensityX = 1;
+    PlaybotLocomotion.intensityX = 1;
   } else if (!left && right) {
-    _motor.intensityX = -1;
+    PlaybotLocomotion.intensityX = -1;
   } else {
-    _motor.intensityX = 0;
+    PlaybotLocomotion.intensityX = 0;
   }
 };
