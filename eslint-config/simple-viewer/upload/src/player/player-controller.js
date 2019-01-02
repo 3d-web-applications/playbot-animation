db312033d2e75e46fdb6c3db5483f3c838d8326a
Lars Wobus <lars.wobus@web.de>
2019-01-02 22:48:32 +0100

    Refactoring: more properties marked as private

--- eslint-config/simple-viewer/upload/src/player/player-controller.js
+++ eslint-config/simple-viewer/upload/src/player/player-controller.js
@@ -26,46 +26,46 @@ PlayerController.prototype.initialize = function () {
   input.register(state.setRight.bind(state), 'KEY_RIGHT');
   input.register(state.setJump.bind(state), 'KEY_SPACE');
 
+  this._playerState = state;
+
   const { _dynamicEntity } = this;
 
-  this.motor = this.entity.script.PlaybotLocomotion;
-  this.motor.setup(_dynamicEntity.rigidbody);
+  this._motor = this.entity.script.PlaybotLocomotion;
+  this._motor.setup(_dynamicEntity.rigidbody);
 
-  this.evaluator = this.entity.script.PlaybotMotionTracking;
-  this.evaluator.setup(_dynamicEntity);
-  this.evaluator._listener.push(this._selectActiveAnimation.bind(this));
+  this._tracking = this.entity.script.PlaybotMotionTracking;
+  this._tracking.setup(_dynamicEntity);
+  this._tracking._listener.push(this._selectActiveAnimation.bind(this));
 
-  this.animationState = -1;
+  this._animationState = -1;
 
-  this.animator = this.entity.script.PlaybotAnimator;
-  this.animator.animation = this._animatedEntity.animation;
-
-  this.playerState = state;
+  this._animator = this.entity.script.PlaybotAnimator;
+  this._animator.animation = this._animatedEntity.animation;
 };
 
 PlayerController.prototype.precision = 0.001;
 
 PlayerController.prototype._selectActiveAnimation = function () {
-  const { dx, dy, dz } = this.evaluator;
+  const { dx, dy, dz } = this._tracking;
   console.log(dx, dy, dz);
   if (Math.abs(dy) > this.precision) {
     console.log(0);
-    if (this.animationState === 0) return;
-    this.animationState = 0;
-    this.animator.startJumpAnimation();
+    if (this._animationState === 0) return;
+    this._animationState = 0;
+    this._animator.startJumpAnimation();
     return;
   }
   if (Math.abs(dx) < this.precision && Math.abs(dz) < this.precision) {
     console.log(2);
-    if (this.animationState === 2) return;
-    this.animationState = 2;
-    this.animator.startIdleAnimation();
+    if (this._animationState === 2) return;
+    this._animationState = 2;
+    this._animator.startIdleAnimation();
     return;
   }
   console.log(1);
-  if (this.animationState === 1) return;
-  this.animationState = 1;
-  this.animator.startRunAnimation();
+  if (this._animationState === 1) return;
+  this._animationState = 1;
+  this._animator.startRunAnimation();
 };
 
 PlayerController.prototype.postInitialize = function () {
@@ -75,26 +75,24 @@ PlayerController.prototype.postInitialize = function () {
 PlayerController.prototype.syncedUpdate = function () {
   const {
     forward, backward, left, right, jump,
-  } = this.playerState;
+  } = this._playerState;
+  const { _motor } = this;
 
-  // console.log(forward, backward, left, right, jump);
-
-  const motor = this.entity.script.PlaybotLocomotion;
   if (forward && !backward) {
-    motor.intensityZ = 1;
+    _motor.intensityZ = 1;
   } else if (!forward && backward) {
-    motor.intensityZ = -1;
+    _motor.intensityZ = -1;
   } else {
-    motor.intensityZ = 0;
+    _motor.intensityZ = 0;
   }
 
-  motor.intensityY = (jump) ? 1 : 0;
+  _motor.intensityY = (jump) ? 1 : 0;
 
   if (left && !right) {
-    motor.intensityX = 1;
+    _motor.intensityX = 1;
   } else if (!left && right) {
-    motor.intensityX = -1;
+    _motor.intensityX = -1;
   } else {
-    motor.intensityX = 0;
+    _motor.intensityX = 0;
   }
 };
